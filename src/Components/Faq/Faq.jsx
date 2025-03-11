c42977375305e47cad24e73ab8b183997c506969
Ajayi Adeoluwa <oluwajre2412@gmail.com>
2025-03-11 09:41:17 +0100

    Added Content X2

--- src/Components/Faq/Faq.jsx
+++ src/Components/Faq/Faq.jsx
@@ -17,12 +17,12 @@ const Faq = () => {
                     <div className="accordion-item shadow mb-3">
                         <h2 className="accordion-header">
                         <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            What are the main features?
+                            What services does Partan offer?
                         </button>
                         </h2>
                         <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
                             <div className="accordion-body">
-                                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                <strong>We provide a range of services</strong>, including <code>Cloud Hosting</code>, <code>Digital Marketing</code>, <code>Web & App Development</code>, <code>Business Consulting</code>, and <code>Branding & Design</code>. Our solutions are tailored to help businesses grow and thrive.
                             </div>
                         </div>
                     </div>
